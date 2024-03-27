59b6e87fae8550e06a9a551cf7c53e78760242c9
john <johnbaek.bjy@gmail.com>
2024-03-27 13:47:30 +0900

    - Update

--- Resources/Proxy/build.sh
+++ Resources/Proxy/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+git fetch origin
+git reset --hard origin/main
+git clean -df
+
 
 # 실행 중인 sgs/budget-bringer-proxy:latest 컨테이너 ID
 container_id=$(docker ps -q -f ancestor=sgs/budget-bringer-proxy:latest)
@@ -15,6 +19,6 @@ echo "Building new Docker image..."
 docker build -t sgs/budget-bringer-proxy .
 
 echo "Starting new container..."
-docker run -d --name budget-bringer-proxy -p --network=sgs-net --restart=unless-stopped --ip=172.28.0.20 -p 8000:80 sgs/budget-bringer-proxy:latest
+docker run -d --name budget-bringer-proxy --network=sgs-net --restart=unless-stopped --ip=172.28.0.20 -p 8000:80 sgs/budget-bringer-proxy:latest
 
 echo "Deployment completed successfully."
