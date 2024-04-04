c1c82a610c2c870bf8cb0439853a77f422d1b8f4
john <johnbaek.bjy@gmail.com>
2024-04-04 15:48:09 +0900

    - 마이그레이션 추가

---
+++ Models/DataModels/MIgrations/20240404054612_04.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.DataModels.Migrations
+{
+    /// <inheritdoc />
+    public partial class _04 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SectorName",
+                table: "BudgetPlans",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SectorName",
+                table: "BudgetApproved",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SectorName",
+                table: "BudgetPlans");
+
+            migrationBuilder.DropColumn(
+                name: "SectorName",
+                table: "BudgetApproved");
+        }
+    }
+}
