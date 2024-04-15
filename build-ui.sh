5c491fb6fd4a5655bf43e6b70bf3d4dd8747f157
john <johnbaek.bjy@gmail.com>
2024-04-15 16:50:31 +0900

    - 빌드 파일 추가

--- build-ui.sh
+++ build-ui.sh
@@ -1,3 +1,47 @@
 git fetch origin
 git reset --hard origin/main
 git clean -df
+
+backup_tag=$(date +"%Y%m%d%H%M%S")
+
+# "sgs/budget-bringer-ui:latest"를 제외한 모든 "sgs/budget-bringer-ui" 이미지를 삭제
+sudo docker images | grep 'sgs/budget-bringer-ui' | grep -v 'latest' | awk '{print $3}' | xargs -r docker rmi
+
+# 실행 중인 "sgs/budget-bringer-ui" 컨테이너를 찾아 정지하고 삭제
+container_id=$(docker ps | grep 'sgs/budget-bringer-ui' | awk '{print $1}')
+if [ ! -z "$container_id" ]; then
+    sudo docker stop $container_id
+    sudo docker rm $container_id
+
+    sudo docker tag sgs/budget-bringer-ui:latest sgs/budget-bringer-ui:$backup_tag
+fi
+
+# 기존 최신 이미지 삭제
+sudo docker rmi sgs/budget-bringer-ui:latest
+
+# 이미지를 새로 빌드
+docker build --no-cache -f BudgetBringer/Dockerfile -t sgs/budget-bringer-ui BudgetBringer/
+sleep 5
+
+
+# builds 폴더가 없으면 생성
+if [ ! -d "builds" ]; then
+    echo "Creating builds directory..."
+    mkdir builds
+fi
+
+# builds 폴더 내의 기존 budget-bringer-ui-*.tar 파일 삭제
+echo "Removing existing budget-bringer-ui-*.tar files..."
+rm -f builds/budget-bringer-ui-*.tar
+
+# 현재 날짜와 시간을 포함하는 새 파일 이름을 생성
+FILENAME="budget-bringer-ui.tar"
+echo "Creating new file: $FILENAME"
+
+# Docker 이미지를 '.tar' 파일로 저장합니다.
+sudo docker save sgs/budget-bringer-ui:latest -o "builds/$FILENAME"
+
+echo "File saved to builds/$FILENAME"
+
+docker run --name budget-bringer-ui --restart=unless-stopped --network=sgs-net --ip=172.28.0.10 -p 8880:80 -d sgs/budget-bringer-ui:latest
+
