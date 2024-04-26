cfacf153cb2d051387b352950ad9a1c1cd101608
john <johnbaek.bjy@gmail.com>
2024-04-26 15:48:07 +0900

    Update Script

--- build-api.sh
+++ build-api.sh
@@ -1,26 +1,23 @@
-cp ./Apis/Dockerfile ./
+#!/bin/bash
 
+# 현재 시간을 기반으로 백업 태그 생성
 backup_tag=$(date +"%Y%m%d%H%M%S")
 
+# "sgs/budget-bringer-api:latest" 이미지를 백업 태그로 저장
+sudo docker tag sgs/budget-bringer-api:latest sgs/budget-bringer-api:$backup_tag
+
 # 실행 중인 "sgs/budget-bringer-api" 컨테이너를 찾아 정지하고 삭제
-container_id=$(docker ps | grep 'sgs/budget-bringer-api' | awk '{print $1}')
+container_id=$(docker ps -q -f name=budget-bringer-api)
 if [ ! -z "$container_id" ]; then
     sudo docker stop $container_id
     sudo docker rm $container_id
-
-    sudo docker tag sgs/budget-bringer-api:latest sgs/budget-bringer-api:$backup_tag
 fi
 
-# "sgs/budget-bringer-api:latest"를 제외한 모든 "sgs/budget-bringer-api" 이미지를 삭제
-sudo docker images | grep 'sgs/budget-bringer-api' | grep -v 'latest' | awk '{print $3}' | xargs -r sudo docker rmi
-
-# 기존 최신 이미지 삭제
+# "sgs/budget-bringer-api:latest" 이미지 삭제
 sudo docker rmi sgs/budget-bringer-api:latest
 
-# 이미지를 새로 빌드
-docker build -t sgs/budget-bringer-api .
-sleep 5
-
+# Docker 이미지 새로 빌드
+sudo docker build -t sgs/budget-bringer-api .
 
 # builds 폴더가 없으면 생성
 if [ ! -d "builds" ]; then
@@ -33,13 +30,15 @@ echo "Removing existing budget-bringer-api-*.tar files..."
 rm -f builds/budget-bringer-api-*.tar
 
 # 현재 날짜와 시간을 포함하는 새 파일 이름을 생성
-FILENAME="budget-bringer-api.tar"
+FILENAME="budget-bringer-api-$backup_tag.tar"
 echo "Creating new file: $FILENAME"
 
-# Docker 이미지를 '.tar' 파일로 저장합니다.
+# Docker 이미지를 '.tar' 파일로 저장
 sudo docker save sgs/budget-bringer-api:latest -o "builds/$FILENAME"
-
 echo "File saved to builds/$FILENAME"
 
+# 컨테이너 재생성
 docker run --name budget-bringer-api -e TZ=Asia/Seoul --restart=unless-stopped -v ./mysql:/var/lib/mysql --network=sgs-net --ip=172.28.0.11 -d sgs/budget-bringer-api:latest
 
+# "sgs/budget-bringer-api" 이미지 중 백업 태그를 제외하고 삭제
+sudo docker images | grep 'sgs/budget-bringer-api' | grep -v $backup_tag | awk '{print $3}' | xargs -r sudo docker rmi
