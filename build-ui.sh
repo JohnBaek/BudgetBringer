4eba2b313b20f5d9cc5053397666c9641f91f253
john <johnbaek.bjy@gmail.com>
2024-04-26 15:54:20 +0900

    - Update

--- build-ui.sh
+++ build-ui.sh
@@ -1,26 +1,26 @@
-cp ./BudgetBringer/Dockerfile ./
+#!/bin/bash
 
+# 현재 시간을 기반으로 백업 태그 생성
 backup_tag=$(date +"%Y%m%d%H%M%S")
 
-# "sgs/budget-bringer-ui:latest"를 제외한 모든 "sgs/budget-bringer-ui" 이미지를 삭제
-sudo docker images | grep 'sgs/budget-bringer-ui' | grep -v 'latest' | awk '{print $3}' | xargs -r docker rmi
+# "sgs/budget-bringer-api:latest" 이미지를 백업 태그로 저장
+sudo docker tag sgs/budget-bringer-api:latest sgs/budget-bringer-api:$backup_tag
 
-# 실행 중인 "sgs/budget-bringer-ui" 컨테이너를 찾아 정지하고 삭제
-container_id=$(docker ps | grep 'sgs/budget-bringer-ui' | awk '{print $1}')
+# 실행 중인 "sgs/budget-bringer-api" 컨테이너를 찾아 정지하고 삭제
+container_id=$(docker ps -q -f name=budget-bringer-api)
 if [ ! -z "$container_id" ]; then
     sudo docker stop $container_id
     sudo docker rm $container_id
-
-    sudo docker tag sgs/budget-bringer-ui:latest sgs/budget-bringer-ui:$backup_tag
 fi
 
-# 기존 최신 이미지 삭제
-sudo docker rmi sgs/budget-bringer-ui:latest
+# Docker 이미지 새로 빌드
+sudo docker build -t sgs/budget-bringer-api .
 
-# 이미지를 새로 빌드
-sudo docker build -t sgs/budget-bringer-ui .
-sleep 5
+# 이미지의 모든 태그 목록 조회 및 latest 태그 제외한 나머지 삭제
+sudo docker images --format '{{.Repository}}:{{.Tag}}' | grep 'sgs/budget-bringer-api' | grep -v 'latest' | xargs -r sudo docker rmi
 
+# 필요한 경우 강제로 이미지 삭제
+# docker rmi -f $(docker images -q sgs/budget-bringer-api)
 
 # builds 폴더가 없으면 생성
 if [ ! -d "builds" ]; then
@@ -28,18 +28,17 @@ if [ ! -d "builds" ]; then
     mkdir builds
 fi
 
-# builds 폴더 내의 기존 budget-bringer-ui-*.tar 파일 삭제
-echo "Removing existing budget-bringer-ui-*.tar files..."
-rm -f builds/budget-bringer-ui-*.tar
+# builds 폴더 내의 기존 budget-bringer-api-*.tar 파일 삭제
+echo "Removing existing budget-bringer-api-*.tar files..."
+rm -f builds/budget-bringer-api-*.tar
 
 # 현재 날짜와 시간을 포함하는 새 파일 이름을 생성
-FILENAME="budget-bringer-ui.tar"
+FILENAME="budget-bringer-api-$backup_tag.tar"
 echo "Creating new file: $FILENAME"
 
-# Docker 이미지를 '.tar' 파일로 저장합니다.
-sudo docker save sgs/budget-bringer-ui:latest -o "builds/$FILENAME"
-
+# Docker 이미지를 '.tar' 파일로 저장
+sudo docker save sgs/budget-bringer-api:latest -o "builds/$FILENAME"
 echo "File saved to builds/$FILENAME"
 
-docker run --name budget-bringer-ui -e TZ=Asia/Seoul --restart=unless-stopped --network=sgs-net --ip=172.28.0.10 -d sgs/budget-bringer-ui:latest
-
+# 컨테이너 재생성
+sudo docker run --name budget-bringer-api -e TZ=Asia/Seoul --restart=unless-stopped -v ./mysql:/var/lib/mysql --network=sgs-net --ip=172.28.0.11 -d sgs/budget-bringer-api:latest
