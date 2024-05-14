8d64d6423b76ba7583428391cf3fa3eed5fa18f9
john <johnbaek.bjy@gmail.com>
2024-05-14 10:08:22 +0900

    - Refactoring

--- BudgetBringer/src/pages/budget/budget-process/budget-process-owner/budget-process-grid-pl-owner-data.ts
+++ BudgetBringer/src/pages/budget/budget-process/budget-process-owner/budget-process-grid-pl-owner-data.ts
@@ -27,10 +27,10 @@ export class BudgetProcessGridPLOwner extends CommonGridModel {
     this.requestQuery = requestQuery;
     this.columDefined = [
       CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "countryBusinessManagerName", date , null,false, false) ,
-      CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "budgetYear", `${year.toString()}FY BudgetYear`, this.numberValueFormatter,false) ,
-      CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "approvedYear", `${year.toString()}FY Approved Amount`, this.numberValueFormatter,false) ,
-      CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "remainingYear", `${year.toString()}FY Remaining Amount`, this.numberValueFormatter,false) ,
-      CommonColumnDefinitions.createColumnDefinitionForNumberFilter(250 , "ratio", `${year.toString()}FY Ratio(%)`, null ,false, function(params) {
+      CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "budgetYear", `Budget Amount`, this.numberValueFormatter,false) ,
+      CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "approvedYear", `Approved Amount`, this.numberValueFormatter,false) ,
+      CommonColumnDefinitions.createColumnDefinitionForTextFilter(250 , "remainingYear", `Remaining Amount`, this.numberValueFormatter,false) ,
+      CommonColumnDefinitions.createColumnDefinitionForNumberFilter(250 , "ratio", `Ratio(%)`, null ,false, function(params) {
         return params.value.toFixed(2);
       }) ,
     ];
